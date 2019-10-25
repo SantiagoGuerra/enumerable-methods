cb7d164d8b489638d3e391c3e8d6c9e4f4e64bd1
Santiago Guerra <guerrasantiago19@gmail.com>
2019-10-25 15:21:59 -0500

    MY_EACH METHOD create my_each method inside Enumerable module

--- main.rb
+++ main.rb
@@ -1,3 +1,12 @@
 module Enumerable
-
+  def my_each
+    container = self.to_a
+    if block_given?
+      (container.size).times do |x|
+        yield(container[x])
+      end
+    else
+      container
+    end
+  end
 end
