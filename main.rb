bc624ead64a53387b2c5582a0e25c7e27dba678f
Santiago Guerra <guerrasantiago19@gmail.com>
2019-10-31 13:02:07 -0500

    RUBOCOP solve issues

--- main.rb
+++ main.rb
@@ -120,8 +120,8 @@ module Enumerable
       container.my_each { |item| aux_container << proc.call(item) }
       aux_container
     end
-    if proc.nil? && block_given? 
-      container.my_each { |item| aux_container << yield(item)}
+    if proc.nil? && block_given?
+      container.my_each { |item| aux_container << yield(item) }
       aux_container
     elsif proc.nil? && !block_given?
       to_enum(:my_map)
@@ -149,8 +149,3 @@ end
 multiply_els([2, 4, 5])
 
 # rubocop:enable Metrics/AbcSize, Metrics/PerceivedComplexity, Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/LineLength, Metrics/ModuleLength, Layout/EndOfLine
-array = [5, 3, 0, 7, 7, 5, 1, 3, 1, 1, 4, 7, 6, 8, 8, 4, 5, 5, 0, 3, 2, 5, 2, 2, 1, 4, 4, 5, 8, 0, 8, 7, 5, 2, 8, 6, 5, 3, 5, 5, 3, 2, 7, 5, 4, 7, 4, 0, 6, 1, 0, 6, 0, 1, 2, 4, 0, 8, 2, 7, 3, 8, 4, 5, 0, 3, 2, 1, 4, 6, 7, 5, 8, 1, 5, 6, 2, 2, 6, 2, 5, 6, 7, 2, 8, 5, 1, 6, 2, 1, 4, 6, 6, 1, 0, 5, 7, 7, 4, 1]
-puts array.my_inject(:+)
-puts array.my_inject(:+)
-puts array.my_inject(:+)
-puts array.my_inject(:+)
