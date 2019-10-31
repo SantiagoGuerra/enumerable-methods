c85da28eeb4f7fdbb7d2d8150ae010883afb67e2
Santiago Guerra <guerrasantiago19@gmail.com>
2019-10-31 12:58:02 -0500

    MY_INJECT METHOD

--- main.rb
+++ main.rb
@@ -130,9 +130,10 @@ module Enumerable
 
   def my_inject(*args)
     container = to_a
-    initial = container.shift
-    container.unshift args[0] if args[0].class != Symbol && !args.empty?
-    container.my_each do |item|
+    aux_container = container.clone
+    initial = aux_container.shift
+    aux_container.unshift args[0] if args[0].class != Symbol && !args.empty?
+    aux_container.my_each do |item|
       initial = yield(initial, item) if block_given?
       initial = initial.send(args[1], item) if args[1].class == Symbol
       initial = initial.send(args[0], item) if args[0].class == Symbol
@@ -147,6 +148,9 @@ end
 
 multiply_els([2, 4, 5])
 
-procc = proc {|x| x + 2}
-
 # rubocop:enable Metrics/AbcSize, Metrics/PerceivedComplexity, Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/LineLength, Metrics/ModuleLength, Layout/EndOfLine
+array = [5, 3, 0, 7, 7, 5, 1, 3, 1, 1, 4, 7, 6, 8, 8, 4, 5, 5, 0, 3, 2, 5, 2, 2, 1, 4, 4, 5, 8, 0, 8, 7, 5, 2, 8, 6, 5, 3, 5, 5, 3, 2, 7, 5, 4, 7, 4, 0, 6, 1, 0, 6, 0, 1, 2, 4, 0, 8, 2, 7, 3, 8, 4, 5, 0, 3, 2, 1, 4, 6, 7, 5, 8, 1, 5, 6, 2, 2, 6, 2, 5, 6, 7, 2, 8, 5, 1, 6, 2, 1, 4, 6, 6, 1, 0, 5, 7, 7, 4, 1]
+puts array.my_inject(:+)
+puts array.my_inject(:+)
+puts array.my_inject(:+)
+puts array.my_inject(:+)
